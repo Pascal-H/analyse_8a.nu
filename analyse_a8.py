515de72aa3fc19eb5473adfb5de417f09210d220
Pascal Hecker <Climbing.Pascal@gmail.com>
2018-08-09 19:53:08 +0200

    Get meta-data from table

--- analyse_a8.py
+++ analyse_a8.py
@@ -31,11 +31,23 @@ conn = sqlite3.connect('../8a_climbing/database.sqlite')
 # Simple query to get a first impression of the table 'user'
 query_distinct = "SELECT * FROM user LIMIT 50;"
 
+# Query to get the meta-info of the table 'user
+query_pragma = "PRAGMA table_info(user);"
+
+
 # Fetch the data from the database and read it into a DataFrame 
 result_distinct = pd.read_sql_query(query_distinct, conn)
 
+# Fetch the meta-data
+result_pragma = pd.read_sql_query(query_pragma, conn)
+
 # Print the header of the DataFrame to get a first impression
 print(result_distinct.head(10))
+
+# Print the meta-data:
+# cid = column id; name = column name; type = data tpye;
+# notnull = must have values; dflt_value = default-value; pk = primary key
+print(result_pragma)
 
 # Close the connection to the SQLite database at the end of the session
 conn.close()
