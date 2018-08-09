9d2c84673dbbd2b846e66cf430f798fe4fd8ce86
Pascal Hecker <Climbing.Pascal@gmail.com>
2018-08-09 23:38:13 +0200

    Implemented functions to fetch all table names and get metadata of all columns

--- analyse_a8.py
+++ analyse_a8.py
@@ -68,6 +68,94 @@ def preview_tables(table_names, limit, connection):
     return fetched_tables
 
 
+def get_table_names(connection):
+    """
+    Get all table names from the connected data base.
+    
+    
+    Parameters
+    ----------
+    connection : SQLite Connection Object
+        Connection object with an open connection to the target 
+        SQLite database.
+    
+    Returns
+    -------
+    table_names : list of strings
+        List with all table names of the connected SQLite database as strings.
+    
+    """
+    
+    ## Query to extract the names of all tables from a database ##
+    # sqlite_master: Master listing of all database objects
+    # Filter by name to get only the names of all tables
+    tables_query = """
+        SELECT name 
+        FROM sqlite_master
+        WHERE type='table'
+        ORDER BY name;
+    """
+    
+    # Save returned table names from query to data frame
+    tables_info = pd.read_sql_query(tables_query, connection)
+    
+    # Extract the table names to the table_names list
+    table_names = []
+    for cur_table_name in tables_info.name:
+        table_names.append(cur_table_name)
+    
+    # Return the list with all table names
+    return table_names
+
+
+def get_column_info(tables_names, connection):
+    """
+    Gets column metadata from the handed over tables.
+    
+    Parameters
+    ----------
+    table_names : list of strings
+        List with strings of the names of tables to fetch data from. 
+    
+    connection : SQLite Connection Object
+        Connection object with an open connection to the target 
+        SQLite database.
+    
+    Returns
+    -------
+    tables_column_infos : dictionary
+        Dictionary with a key for each table and the respective column metadata
+        contained as a Data Frame.
+    
+    """
+    
+    # Dinctionary to contain the table name and column metadata key-value pairs
+    tables_column_infos = {}
+    # Fetch the column metadata for each table name in the list
+    # PRAGMA: SQLite specific meta commands and queries
+    # table_info: returns all configuration data on the column of each table
+    for cur_table in tables_names:
+        columns_query = """
+            PRAGMA table_info({name});
+        """.format(
+            name = cur_table)
+        
+        # Fetch the column metadata of the current table with the current query
+        # and save it to a DataFrame
+        cur_column_info = pd.read_sql_query(columns_query, connection)
+        
+        # For the 8a.nu data set: cid = column index is the first column
+        # Set this as the index of the DataFrame
+        cur_column_info = cur_column_info.set_index('cid')
+        
+        # Use the name of the current talbe as the key in the output dictionary
+        # and assign the DataFrame with the respective metadata of the columns
+        tables_column_infos[cur_table] = cur_column_info
+    
+    # Return the dictionary with all table names and column metadata
+    return tables_column_infos
+
+
 ### Main ###
 
 # Get the data set, before running the script in Python:
@@ -113,6 +201,34 @@ preview_list = preview_tables(table_names, limit, conn)
 # Print the head of all data sets of all tables in the 8a.nu database
 for cur_table_data in preview_list:
     print(cur_table_data.head())
+
+
+## Automatically fetch all table names and get the column metadata ##
+
+# Get list with all table names from the connected database
+table_names_lst = get_table_names(conn)
+
+# Get dictionary assigning the column metadata to every table name
+tables_dct = get_column_info(table_names_lst, conn)
+
+# Print the column attributes from the 'first' entry of the dictionary
+column_attributes = list(tables_dct["ascent"].columns.values)
+print(column_attributes)
+# Legend for the abreviation for the column metadata labels
+attributes_legend =  "Legend for the column attributes:\n"
+attributes_legend += "cid:        column identifier\n"
+attributes_legend += "name:       column name\n"
+attributes_legend += "type:       data type\n"
+attributes_legend += "notnull:    must contain values\n"
+attributes_legend += "dflt_value: default value\n"
+attributes_legend += "pk:         primary key marker\n\n"
+print(attributes_legend)
+
+# Print the Table name (key) and the column metadata (value) for all tables
+for cur_table in tables_dct.items():
+    print("Table Name: " + str(cur_table[0]))
+    print("Column Data:\n" + str(cur_table[1]) + "\n\n")
+
 
 # Close the connection to the SQLite database at the end of the session
 conn.close()
